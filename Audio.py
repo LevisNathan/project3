38aa969de53a30ecb627d7a7a864dd360ab0320e
Nathan Levis <levisnathan@gmail.com>
2016-11-14 00:31:57 -0800

    Update Audio.py
    This has examples on the pause, play, stop, and volume controls

--- Audio.py
+++ Audio.py
@@ -1,6 +1,7 @@
 import os
 import pygame
-
+import sys
+from pygame.locals import *
 
 Folder = 'C:/tmp/MediaPLayer/'
 
@@ -16,20 +17,44 @@ def L():
 
 
 def Audio():
+    pressed = False
+    vol = .75
     pygame.mixer.pre_init(44100, 16, 2, 4096)
     #  frequency, size, channels, bufferSize
     pygame.init()  # turn all of pyGame on.
     pygame.mixer.music.load('C:/tmp/MediaPLayer/BMA.wav')
-    pygame.display.set_mode((200, 100))
+    pygame.display.set_mode((400, 100))
+    # screen = pygame.display.set_mode((640, 480))
+    # pygame.draw.rect(screen, (255, 0, 0), {300, 75}, 0)
+    pygame.display.set_caption("Music Player")
     clock = pygame.time.Clock()
     pygame.mixer.music.play()
     clock.tick(10)
+
     while pygame.mixer.music.get_busy():
         pygame.event.poll()
-        clock.tick(10)
+        pess = pygame.key.get_pressed()
+        if pess[K_1]:
+            vol += .05
+            print(pygame.mixer.music.get_volume())
+        if pess[K_2]:
+            vol -= .05
+            print(pygame.mixer.music.get_volume())
 
+        if pess[K_DELETE]:
+            pygame.mixer.music.pause()
+
+        if pess[K_BACKSPACE]:
+            pygame.mixer.music.unpause()
+
+        if pess[K_INSERT]:
+            pygame.quit()
+            sys.exit(0)
+        pygame.mixer.music.set_volume(vol)
+        clock.tick(10)
 
 fMaker()
 L()
 Audio()
-
+pygame.quit()
+sys.exit(0)
